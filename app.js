f60facadab3b8a43ed6cbb0e4c6315a75faa489f
Atika Sultana <atika72rahman@gmail.com>
2021-02-08 20:42:40 +0600

    second commit

--- app.js
+++ app.js
@@ -1,62 +1,58 @@
-const searchButton = document.getElementById("search-button");
-searchButton.addEventListener("click", () => {
-    showMeal()
-})
+const searchFoods = () => {
+    const food = document.getElementById("search-box").value;
+     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`
+     fetch(url)
+     .then(res => res.json())
+     .then(data => displayFoods(data.meals));
+} 
 
-function showMeal() {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${'food'}`)
-        .then(res => res.json())
-        .then(data => displayFoods(data.meals));
+const displayFoods = foods => {
+    const divFoods = document.getElementById('foods');
 
-    const displayFoods = foods => {
-        const divFoods = document.getElementById('foods');
+    foods.forEach(food => {
+        const divFood = document.createElement('div');
 
-        foods.forEach(food => {
-            const divFood = document.createElement('div');
+        divFood.className = 'food';
+        const foodInfo = `
+        <div id="showIngredients">
+        <img class="image" src = ${food.strMealThumb}>
+        <h3 class="food-name">${food.strMeal}</h3>
+        </div>
+        <p id="ingredients">${food.strInstructions}<p>
+        
+        `
+        divFood.innerHTML = foodInfo;
+        divFoods.appendChild(divFood);
 
-            divFood.className = 'food';
-            const foodInfo = `
-            <img class="image" src = ${food.strMealThumb}>
-            <h3 class="food-name">${food.strMeal}</h3>
-            <button onclick="displayFoodInstruction('${food.strInstructions}')"> Details </button>
-            `
-            divFood.innerHTML = foodInfo;
-            divFoods.appendChild(divFood);
-        });
+        
+const ingredientList = document.getElementById("showIngredients");
+        ingredientList.addEventListener("click", (){
+            const showList = document.getElementById("ingredients");
+        }) 
     }
-
-    const displayFoodInstruction = instruction => {
-        console.log(instruction);
-    }
-
-}
+    
+       
+)}
 
 
 
-// const apiKey = '1';
-// fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
-// .then(res => res.json())
-// .then(data => displayFoods(data.meals));
-
-// const displayFoods = foods => {
-//     const divFoods = document.getElementById('foods');
-
-//     foods.forEach(food => {
-//         const divFood = document.createElement('div');
-
-//         divFood.className = 'food';
-//         const foodInfo = `
-
-//             <img class="image" src = ${food.strMealThumb}>
-//             <h3 class="food-name">${food.strMeal}</h3>
-//             <button onclick="displayFoodInstruction('${food.strInstructions}')"> Details </button>
-//         `
-//         divFood.innerHTML = foodInfo;
-//         divFoods.appendChild(divFood);
-//     });
+// const displayRecipe= (ingredients) => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${ingredients}`
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => console.log(data.strInstructions))
 // }
 
-// const displayFoodInstruction = instruction => {
-//     console.log(instruction);
-// }
 
+// const displayFoodInstruction = meals => {
+//     console.log(meals);
+// }
+//  <button onclick="displayFoodInstruction('${food.strInstructions}')"> Details </button>
+
+
+// const ingredientList = document.getElementById("showIngredients");
+//         ingredientList.addEventListener("click", (){
+//             const showList = document.getElementById("ingredients");
+//         }) 
+
+//          
